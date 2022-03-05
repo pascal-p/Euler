68c12e9f2fd0b9b2ae5da8532b083cac3f017337
Pascal P <lacsap_666@yahoo.fr>
2022-03-05 20:06:05 +1300

    a faster method
    * mapping factorial integers and the number with n digits in
      their factorial representation

--- Lexicographic_permutations/lexicographic-perms-exp.jl
+++ Lexicographic_permutations/lexicographic-perms-exp.jl
@@ -17,8 +17,10 @@ nth_perm(10, 999_999)  # 999_999th permutaion of sequence 0..9
 ```
 """
 function nth_perm(limit::UInt, p::UInt)::String
-  @assert(limit ≤ LIMIT &&  p ≤ FACT_20,
+  @assert(limit ≤ LIMIT && p ≤ FACT_20,
           "limit: $(limit) ≤ $(LIMIT) && p $(p) ≤ $(FACT_20)")
+  @assert(p ≤ Base.factorial(limit), "p should nbe less than factorial(limit)")
+
   perm = collect(0:limit - 1)
   n = length(perm) |> UInt
 
@@ -44,9 +46,113 @@ function nth_perm(limit::UInt, p::UInt)::String
 end
 
 function nth_perm(limit::Integer, p::Integer)::String
-  @assert limit > 0 && p > 0
+  @assert(limit > 0 && p > 0, "limit and p must be natural integers")
 
   nth_perm(UInt(limit), UInt(p))
 end
 
 @inline swap!(v::Vector{UInt}, ix::UInt, jx::UInt) = v[ix], v[jx] = v[jx], v[ix]
+
+"""
+   Alternative - using factorial number system (cf. https://en.wikipedia.org/wiki/Factorial_number_system)
+
+The method is described here: https://en.wikipedia.org/wiki/Factorial_number_system#Permutations
+
+ex. 1_000_000 in factorial number system is 2662512200
+now start with the digit (say d) at the rightmost position, prepend it to prev. digits after incrementing all
+these digits if they are ≥ d
+
+          0   - start with 0
+         01   - next prepend 0 to (0+1)
+        201   - next prepend 2 to 01 (no inc, because both 0 and 1 < 2)
+       2301   - next prepend 2 to 2+1 0 1
+      13402   - next prepend 1 to 2+1 3+1 0 1+1
+     513402   ...
+    2614503
+   62714503
+  672814503
+ 2783915604
+
+
+Other way, take 2662512200 and the set (to permut.) 0123456789
+
+  ix: 012              (index is 2 starting at 0, hence 012)
+perm: 0123456789 -> 2
+  ix: 0123456          (index is 6 starting at 0, hence 0123456)
+perm: 013456789  -> 7
+  ix: 0123456          (index is 6 starting at 0, hence 0123456)
+perm: 01345689   => 8
+  ix: 012              ...
+perm: 0134569    => 3
+  ix: 012345           (index is 5 starting at 0, hence 012345)
+perm: 014569     => 9
+  ix: 01               ...
+perm: 01456      => 1
+  ix: 012              ...
+perm: 0456       => 5
+  ix: 012              ...
+perm: 046        => 6
+  ix: 0                ...
+perm: 04         => 4
+  ix: 0                ...
+perm: 0          => 0
+
+"""
+
+function nth_perm_alt1(limit::UInt, p::UInt)::String
+  @assert(p ≤ Base.factorial(limit), "p should be less than factorial(limit)")
+  @assert(limit ≤ LIMIT && p ≤ FACT_20,
+          "limit: $(limit) ≤ $(LIMIT) && p $(p) ≤ $(FACT_20)")
+
+  n_fb = to_factorial_base(p)
+  perm = zeros(UInt, limit)
+  ix = limit
+
+  for sd ∈ length(n_fb):-1:1
+    d = parse(UInt, n_fb[sd])
+
+    for jx ∈ limit:-1:ix
+      perm[jx] ≥ d && (perm[jx] += 1)
+    end
+
+    perm[ix] = d
+    ix -= 1
+  end
+
+  join(perm)
+end
+
+function nth_perm_alt1(limit::Integer, p::Integer)::String
+  limit > 0 && p > 0 && (return nth_perm_alt1(UInt(limit), UInt(p)))
+  throw(ArgumentError("Expecting limit, p to be natural integers"))
+end
+
+"""
+   to_factorial_base(p::UInt)::String
+
+Convert given p into factorial base system (expressed as a string)
+
+Examples
+```julia-repl
+to_factorial_base(1_000_000)
+"2662512200"
+```
+"""
+function to_factorial_base(p::UInt)::String
+  p > FACT_20 && throw(ArgumentError("Limit is fixed at $(FACT_20)"))
+
+  d = one(UInt)
+  s = ""
+  while p > zero(UInt)
+    p, r = divrem(p, d)
+    s = string(r, s)
+    d += one(UInt)
+  end
+
+  s
+end
+
+function to_factorial_base(p::Integer)::String
+  (p < 0) && throw(ArgumentError("Expecting p to be a natural integer"))
+  to_factorial_base(UInt(p))
+end
--- Lexicographic_permutations/runtests-lpe.jl
+++ Lexicographic_permutations/runtests-lpe.jl
@@ -3,31 +3,78 @@ using Test
 include("lexicographic-perms-exp.jl")
 
 
-@testset "#all_perms_nr" begin
-  @test all_perms(10, 699_999)   == "1938246570"
-  @test all_perms(10, 899_999)   == "2536987410"
-  @test all_perms(10, 900_000)   == "2537014689"
-  @test all_perms(10, 999_999)   == "2783915460"
-  @test all_perms(10, 1_000_000) == "2783915604"
-  @test all_perms(10, 2_000_000) == "5468731209"
-  @test all_perms(10, 3_000_000) == "8241703569"
+@testset "#nth_perms" begin
+  @test nth_perm(10, 699_999)   == "1938246570"
+  @test nth_perm(10, 899_999)   == "2536987410"
+  @test nth_perm(10, 900_000)   == "2537014689"
+  @test nth_perm(10, 999_999)   == "2783915460"
+  @test nth_perm(10, 1_000_000) == "2783915604"
+  @test nth_perm(10, 2_000_000) == "5468731209"
+  @test nth_perm(10, 3_000_000) == "8241703569"
 end
+
+@testset "Exception with nth_perms" begin
+  @test_throws AssertionError nth_perm(-10, 2_000)
+
+  @test_throws AssertionError nth_perm(3, 10) # 0, 1, 2 has only 6 permutations
+end
+
+@testset "#nth_perms_alt1" begin
+  @test nth_perm_alt1(10, 699_999)   == "1938246570"
+  @test nth_perm_alt1(10, 899_999)   == "2536987410"
+  @test nth_perm_alt1(10, 900_000)   == "2537014689"
+  @test nth_perm_alt1(10, 999_999)   == "2783915460"
+  @test nth_perm_alt1(10, 1_000_000) == "2783915604"
+  @test nth_perm_alt1(10, 2_000_000) == "5468731209"
+  @test nth_perm_alt1(10, 3_000_000) == "8241703569"
+
+  @test nth_perm_alt1(7, 2982) == "4062135" # 2982nd perm of 0..6
+end
+
+@testset "Exception with nth_perms" begin
+  @test_throws ArgumentError nth_perm_alt1(10, -2_000)
+
+  @test_throws AssertionError nth_perm_alt1(5, 1000) # 0, 1, 2, 3, 4 has only 120 permutations
+end
+
+@testset "#to_factorial_base" begin
+  @test to_factorial_base(463) == "341010"
+  @test to_factorial_base(2982) == "4041000"
+
+  @test to_factorial_base(1_000_000) == "2662512200" # "2662512110"
+end
+
+
+## Timing
 
 # because of order - need to a prep for the 1_000_000th
 @testset  "Prep. for performance timing" begin
-  @test_skip all_perms(10, 3_000_000)
-  @test_skip all_perms(10, 1_000_000)
-  @test_skip all_perms(10, 2_000_000)
+  @test_skip nth_perm(10, 3_000_000)
+  @test_skip nth_perm(10, 1_000_000)
+  @test_skip nth_perm(10, 2_000_000)
 end
 
-@time @testset "1_000_000-th permutation of 10 digits" begin
-  @test all_perms(10, 1_000_000) == "2783915604"
+@time @testset "nth_perm: 1_000_000-th permutation of 10 digits" begin
+  @test nth_perm(10, 1_000_000) == "2783915604"
 end
 
-@time @testset "2_000_000-th permutation of 10 digits" begin
-  @test all_perms(10, 2_000_000) == "5468731209"
+@time @testset "nth_perm: 2_000_000-th permutation of 10 digits" begin
+  @test nth_perm(10, 2_000_000) == "5468731209"
 end
 
-@time @testset "3_000_000-th permutation of 10 digits" begin
-  @test all_perms(10, 3_000_000) == "8241703569"
+@time @testset "nth_perm: 3_000_000-th permutation of 10 digits" begin
+  @test nth_perm(10, 3_000_000) == "8241703569"
+end
+
+
+@time @testset "nth_perm_alt1: 1_000_000-th permutation of 10 digits" begin
+  @test nth_perm_alt1(10, 1_000_000) == "2783915604"
+end
+
+@time @testset "nth_perm_alt1: 2_000_000-th permutation of 10 digits" begin
+  @test nth_perm_alt1(10, 2_000_000) == "5468731209"
+end
+
+@time @testset "nth_perm_alt1: 3_000_000-th permutation of 10 digits" begin
+  @test nth_perm_alt1(10, 3_000_000) == "8241703569"
 end
